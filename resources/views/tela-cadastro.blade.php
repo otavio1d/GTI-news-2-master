64e1a5844a171a2fd1b09bc672a52b0f2c407ab1
otavio1d <otaviordias77@gmail.com>
2024-12-04 19:23:28 -0300

    final

--- resources/views/tela-cadastro.blade.php
+++ resources/views/tela-cadastro.blade.php
@@ -14,7 +14,7 @@
     <input type="email" name="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Digite seu email" required />
   </div>
   <div class="mb-5">
-    <label for="senha" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sua senha</label>
+    <label for="senha" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sua senha.</label>
     <input type="password" name="senha" id="senha" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Digite sua senha" required />
   </div>
   <div class="mb-5">
